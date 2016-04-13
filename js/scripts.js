e2cad8e81b9533403a03b6bd77ae253ee96a9055
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-12 20:10:46 -0700

    remove words over 3 characters long and reverse string

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
 $("#listenToMe").click(function() {
-  $("#myWords").text($("#yourWords").val());
+  var iHearYou = [];
+  iHearYou=$("#yourWords").val().split(" ");
+  var whatIHeard = [];
+  iHearYou.forEach(function(word) {
+    if(word.length > 3) {
+      whatIHeard.push(word.toUpperCase());
+    }
+  });
+//loop through to build a new array out of all words over 3 letters long
+  whatIHeard.reverse();
+  $("#myWords").text(whatIHeard.join(" "));
   $("#response").toggle();
   $("#listening").toggle();
 });
