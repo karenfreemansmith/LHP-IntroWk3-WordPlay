2da303201336652426b9234ad0681b19b45dd6ab
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-12 19:55:07 -0700

    show/hide page when buttons are clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
 $("#listenToMe").click(function() {
   $("#myWords").text($("#yourWords").val());
+  $("#response").toggle();
+  $("#listening").toggle();
+});
+
+$("#tryAgain").click(function() {
+  $("#yourWords").val("");
+  $("#response").toggle();
+  $("#listening").toggle();
 });
